e84f8cf451107d2dd537f7eac9b31b3b59adb361
Maarten Oelering <maarten@brightcode.nl>
2013-01-22 11:03:20 +0100

    bundle generated

---
+++ lib/smtp-server.rb
@@ -0,0 +1,7 @@
+require "smtp-server/version"
+
+module Smtp
+  module Server
+    # Your code goes here...
+  end
+end
---
+++ lib/smtp-server/version.rb
@@ -0,0 +1,5 @@
+module Smtp
+  module Server
+    VERSION = "0.0.1"
+  end
+end
